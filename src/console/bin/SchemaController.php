db015e0584469c3d4ef5dd34b1a8dda01bbd2dee
Vitaliy <theyamshikov@yandex.ru>
2019-07-30 14:30:50 +0600

    feat: make remove schemas

--- src/console/bin/SchemaController.php
+++ src/console/bin/SchemaController.php
@@ -5,6 +5,8 @@ namespace yii2bundle\db\console\bin;
 use yii2lab\db\domain\enums\DbDriverEnum;
 use yii2rails\app\domain\helpers\EnvService;
 use yii2rails\extension\console\helpers\Error;
+use yii2rails\extension\console\helpers\input\Enter;
+use yii2rails\extension\console\helpers\input\Question;
 use yii2rails\extension\console\helpers\input\Select;
 use yii2rails\extension\console\helpers\Output;
 
@@ -40,6 +42,39 @@ class SchemaController extends \yii\base\Component
             \Yii::$app->db->createCommand($sql)->execute();
         }
         Output::title('All schemas created');
+        Output::arr($schemas);
+    }
+
+    public function actionRemove()
+    {
+        Question::confirm('Remove all schemas?', 1);
+        $db = EnvService::getServer('db');
+        if($db['main']['driver'] != DbDriverEnum::PGSQL) {
+            Error::fatal('DB driver need Postgres!');
+        }
+        if(empty($db['main']['map'])) {
+            Error::fatal('Empty map!');
+        }
+        $schemas = [];
+        foreach($db['main']['map'] as $value) {
+            $arr = explode(DOT, $value);
+            if(count($arr) > 1) {
+                $schemas[] = $arr[0];
+            }
+        }
+        $schemas[] = $db['main']['defaultSchema'];
+        $schemas = array_unique($schemas);
+        $schemas = array_values($schemas);
+        foreach ($schemas as $schema) {
+            $sql = 'DROP SCHEMA "' . $schema . '" CASCADE';
+            try {
+                \Yii::$app->db->createCommand($sql)->execute();
+                Output::line('DROP SCHEMA "' . $schema . '"');
+            } catch (\Exception $e) {
+                Output::line('DROP SCHEMA "' . $schema . '" Fail');
+            }
+        }
+        Output::title('All schemas deleted');
         Output::arr($schemas);
     }
 
